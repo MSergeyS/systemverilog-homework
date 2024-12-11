cb3e05eca1c747b8ee2cdf0d81e1b19020b87b41
MSergeyS <mosolov.s.s@yandex.ru>
2024-12-11 17:04:14 +0300

    правки по разбору д/з 2

--- 02_sequential_basics/02_07_halve_tokens/02_07_halve_tokens.sv
+++ 02_sequential_basics/02_07_halve_tokens/02_07_halve_tokens.sv
@@ -19,35 +19,52 @@ module halve_tokens
     // a -> 110_011_101_000_1111
     // b -> 010_001_001_000_0101
 
-    // States
-    typedef enum logic
-    {
-      st_first   = 1'b0,
-      st_second  = 1'b1
-    }  statetype_t;
-    statetype_t state, new_state;
+    logic path_true;
 
-    // State transition logic  (логика переходоа конечного автомата)
-    always_comb
-      begin
-        new_state = state;
-
-        case (state)
-          st_first   : if (a) new_state = st_second;
-          st_second  : if (a) new_state = st_first;
-        endcase
-
-      // verilator lint_on  CASEINCOMPLETE
-      end
-
-    // Output logic (обновление выходов)
-    assign b = a & (state == st_second);
-
-    // обновление состояния
-    always_ff @ (posedge clk)
+    always_ff @(posedge clk)
+    begin
       if (rst)
-        state <= st_first;
-      else
-        state <= new_state;
+        begin
+          path_true <= 1'b0;
+        end
+      else if (a)
+        begin
+          path_true <= ~path_true;
+        end
+    end
+
+    assign b = path_true & a;
+
+    // можно написать ввиде конечного автомата (это мой вариант решение)
+    // // States
+    // typedef enum logic
+    // {
+    //   st_first   = 1'b0,
+    //   st_second  = 1'b1
+    // }  statetype_t;
+    // statetype_t state, new_state;
+
+    // // State transition logic  (логика переходоа конечного автомата)
+    // always_comb
+    //   begin
+    //     new_state = state;
+
+    //     case (state)
+    //       st_first   : if (a) new_state = st_second;
+    //       st_second  : if (a) new_state = st_first;
+    //     endcase
+
+    //   // verilator lint_on  CASEINCOMPLETE
+    //   end
+
+    // // Output logic (обновление выходов)
+    // assign b = a & (state == st_second);
+
+    // // обновление состояния
+    // always_ff @ (posedge clk)
+    //   if (rst)
+    //     state <= st_first;
+    //   else
+    //     state <= new_state;
 
 endmodule
--- 02_sequential_basics/02_08_double_tokens/02_08_double_tokens.sv
+++ 02_sequential_basics/02_08_double_tokens/02_08_double_tokens.sv
@@ -7,7 +7,7 @@ module double_tokens
     input        clk,
     input        rst,
     input        a,
-    output       b,
+    output logic b,
     output logic overflow
 );
     // Task:
@@ -25,53 +25,87 @@ module double_tokens
     // a -> 10010011000110100001100100
     // b -> 11011011110111111001111110
 
-    logic [8:0]counter;  // счётчик единиц, которые ещё надо выставить на входе b
+    // решение из разбора д/з
+    logic [7:0] cnt;
 
-    // States
-    typedef enum logic[1:0]
-    {
-      st_stop      = 2'b00,
-      st_increment = 2'b01,
-      st_decrement = 2'b10,
-      st_overflow  = 2'b11
-    } statetype_t;
-    statetype_t state, new_state;
-
-    // State transition logic  (логика переходоа конечного автомата)
-    always_comb
-      begin
-        new_state = state;
-
-        case (state)
-          st_stop      : if ( a) new_state = st_increment;
-          st_increment : if (~a) new_state = st_decrement;
-                         else if (counter > 'd199) new_state = st_overflow;
-          st_decrement : if ( a) new_state = st_increment;
-                         else if ( counter == 'd1) new_state = st_stop;
-        endcase
-      end
-
-    // Output logic (обновление выходов)
-    assign b = (state == st_decrement) | (state == st_increment);
-    assign overflow = (state == st_overflow);
-
-    // обновление состояния
+    // т.к. в задании сказано - поднимать флаг, если "1" > 200 => флаг поднимаем,
+    // когда cnt > 200.
+    // Но также всё работает при поднятии флага при cnt >= 200.
     always_ff @ (posedge clk)
+        if (rst)
+            overflow <= 1'b0;
+        else if (cnt == 'd200 & a)
+            overflow <= 1'b1;
+
+    always_ff @(posedge clk)
       if (rst)
-        state <= st_stop;
-      else
-        state <= new_state;
-
-    // счётчик
-    always_ff @ (posedge clk)
-      if(rst)
-        counter <= 'd0;
-      else
         begin
-          if (state == st_increment)
-            counter <= counter + 'd1;
-          else if (state == st_decrement)
-            counter <= counter - 'd1;
+          cnt <= 'd0;
+          b   <= 'b0;
         end
+      else if (~overflow)
+        if (a)
+          begin
+            cnt <= cnt + 'd1;
+            b   <= 'b1;
+          end
+        else
+          if (cnt >'d0)
+            begin
+              cnt <= cnt - 'd1;
+              b   <= 'b1;
+            end
+          else
+            b   <= 'b0;
+
+    // // моё вариант решения (использовал конечный автомат)
+    // logic [8:0]counter;  // счётчик единиц, которые ещё надо выставить на входе b
+
+    // // States
+    // typedef enum logic[1:0]
+    // {
+    //   st_stop      = 2'b00,
+    //   st_increment = 2'b01,
+    //   st_decrement = 2'b10,
+    //   st_overflow  = 2'b11
+    // } statetype_t;
+    // statetype_t state, new_state;
+
+    // // State transition logic  (логика переходоа конечного автомата)
+    // always_comb
+    //   begin
+    //     new_state = state;
+
+    //     case (state)
+    //       st_stop      : if ( a) new_state = st_increment;
+    //       st_increment : if (~a) new_state = st_decrement;
+    //                      else if (counter > 'd199) new_state = st_overflow;
+    //       st_decrement : if ( a) new_state = st_increment;
+    //                      else if ( counter == 'd1) new_state = st_stop;
+    //     endcase
+    //   end
+
+    // // Output logic (обновление выходов)
+    // assign b = (state == st_decrement) | (state == st_increment);
+    // assign overflow = (state == st_overflow);
+
+    // // обновление состояния
+    // always_ff @ (posedge clk)
+    //   if (rst)
+    //     state <= st_stop;
+    //   else
+    //     state <= new_state;
+
+    // // счётчик
+    // always_ff @ (posedge clk)
+    //   if(rst)
+    //     counter <= 'd0;
+    //   else
+    //     begin
+    //       if (state == st_increment)
+    //         counter <= counter + 'd1;
+    //       else if (state == st_decrement)
+    //         counter <= counter - 'd1;
+    //     end
 
 endmodule
